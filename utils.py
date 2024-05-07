35e409ed9c5a0e6540759d93154fd05c8f7785ba
aipacino123 <jyothishankit@gmail.com>
2024-05-07 11:37:09 -0500

    Added changes for utils and conformer

--- utils.py
+++ utils.py
@@ -331,3 +331,28 @@ class FeedForward(nn.Module):
         )
     def forward(self, x):
         return self.net(x)
+    
+class ConformerConvModule(nn.Module):
+    def __init__(
+        self, dim, causal=False, expansion_factor=2, kernel_size=31, dropout=0.0
+    ):
+        super().__init__()
+        inner_dim = dim * expansion_factor
+        padding = calc_same_padding(kernel_size) if not causal else (kernel_size - 1, 0)
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            Rearrange(CONFORMER_FREQUENCY_REARRANGEMENT),
+            nn.Conv1d(dim, inner_dim * 2, 1),
+            GLU(dim=1),
+            DepthWiseConv1d(
+                inner_dim, inner_dim, kernel_size=kernel_size, padding=padding
+            ),
+            nn.BatchNorm1d(inner_dim) if not causal else nn.Identity(),
+            Swish(),
+            nn.Conv1d(inner_dim, dim, 1),
+            Rearrange(CONFORMER_TIME_REARRANGEMENT),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
